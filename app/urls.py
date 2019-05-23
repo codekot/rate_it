3072cf3f1bb8ed7a6849ca725feb7751e8d825bf
sean <boobookish@gmail.com>
2019-05-23 17:30:08 +0400

    Add image to post request. Add saving image to google bucket

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,7 @@
-from app.views import ItemList, SearchItem, Item, Images
+from app.views import ItemList, SearchItem, Item
 
 urls = {
     '/items/': ItemList,
     '/items/<int:item_id>/': Item,
     '/search/<string:name>': SearchItem,
-    '/images/': Images,
 }
