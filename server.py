a3f6010e388eb7d4af6d5a694a37a86d621f3bdd
egvo <gvozdikov@like-coffee.ru>
2019-05-24 15:29:16 +0400

    Удалил комментарий Настроить CORS.

--- server.py
+++ server.py
@@ -6,5 +6,3 @@ from config import DEBUG, HOST, PORT
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=DEBUG)
     app.url_map.strict_slashes = False
-
-    # @todo: настроить CORS
