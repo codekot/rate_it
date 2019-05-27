dbfe481119fde3bf08af816988170320ffa2b956
egvo <gvozdikov@like-coffee.ru>
2019-05-27 18:50:18 +0400

    Добавил метод удаления записи.

--- app/views.py
+++ app/views.py
@@ -21,6 +21,18 @@ class Item(Resource):
         else:
             return {"message": "Item not found"}, 404
 
+    def delete(self, item_id):
+        item = ItemModel.find_by_id(item_id)
+        if item:
+            try:
+                db.session.delete(item)
+                db.session.commit()
+            except:
+                return {'message': 'Error writing in database'}, 500
+            return {}, 200
+        else:
+            return {"message": "Item not found"}, 404
+
 
 class ItemList(Resource):
 
