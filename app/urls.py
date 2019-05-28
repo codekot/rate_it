93e570addfba36056a8ac74d076bf30401331f53
sean <boobookish@gmail.com>
2019-05-28 18:01:39 +0400

    Add /register/ and UserRegister resource

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
-from app.views import ItemList, SearchItem, Item, UserLogin
+from app.views import ItemList, SearchItem, Item, UserLogin, UserRegister
 
 urls = {
     '/items/': ItemList,
     '/items/<int:item_id>/': Item,
     '/search/<string:name>': SearchItem,
     '/login/': UserLogin,
+    '/register/': UserRegister,
 }
--- app/views.py
+++ app/views.py
@@ -145,6 +145,23 @@ class SearchItem(Resource):
                 result.append(item.json())
         return {'items': result}
 
+
+class UserRegister(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str, required=True, help="Username field (required)")
+        parser.add_argument('password', type=str, required=True, help="Password field (required)")
+        args = parser.parse_args()
+
+        if UserModel.find_by_username(args['username']):
+            return {"message": "A user with that username already exist"}, 400
+
+        user = UserModel(username=args['username'])
+        user.set_password(args['password'])
+        user.save_to_db()
+
+        return {"message": "User created successfully"}, 201
+
 class UserLogin(Resource):
     def post(self):
         parser = reqparse.RequestParser()
