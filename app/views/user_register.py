847f27d1f58341af3e7ee17e8f3a540dffb66ad6
egvo <gvozdikov@like-coffee.ru>
2019-07-01 12:23:16 +0400

    Реализовал валидацию UserRegister с помощью jsonschema.

--- app/views/user_register.py
+++ app/views/user_register.py
@@ -1,22 +1,30 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 
 from app.models import UserModel
-from app.utils import non_empty_string
+from utils.jsonschema_patterns import nonempty_string, email_type
+from utils.validation_decorator import validate_request_json
+
+SCHEMA = {
+    "type": "object",
+    "properties": {
+        "username": nonempty_string,
+        "password": nonempty_string,
+        "email": email_type,
+    },
+    "required": ['username', 'password']
+}
 
 
 class UserRegister(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('username', type=non_empty_string, required=True, help="Username field is required")
-        parser.add_argument('password', type=str, required=True, help="Password field is required")
-        parser.add_argument('email', type=str, help="Email is used for password reset only")
-        args = parser.parse_args()
-
-        if UserModel.find_by_username(args['username']):
+    @validate_request_json(SCHEMA)
+    def post(self, json):
+        if UserModel.find_by_username(json['username']):
             return {"error": {"username": "A user with that username already exist"}}, 400
 
-        user = UserModel(username=args['username'])
-        user.set_password(args['password'])
+        user = UserModel(username=json['username'])
+        user.set_password(json['password'])
+        if json.get('email'):
+            user.email = json['email']
         user.save_to_db()
 
         return {"message": "User created successfully"}, 201
