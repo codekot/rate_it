55e9067186725e999072757fcf41ee5e70605c88
egvo <gvozdikov@like-coffee.ru>
2019-07-01 11:54:59 +0400

    Добавил в модель User email.

--- app/models.py
+++ app/models.py
@@ -94,6 +94,7 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), nullable=False)
     password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(120), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
