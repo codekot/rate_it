17ab53ba1422eae73b6f31eaaf4b4e5731d9b18c
egvo <gvozdikov@like-coffee.ru>
2019-07-02 17:21:53 +0400

    Задал tablename для user.

--- app/models.py
+++ app/models.py
@@ -95,6 +95,8 @@ class CategoryModel(db.Model):
 
 
 class UserModel(db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), nullable=False)
     password_hash = db.Column(db.String(128))
