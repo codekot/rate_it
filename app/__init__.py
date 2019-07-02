5fcde637ee90093394e431df2a869691345f9e5f
egvo <gvozdikov@like-coffee.ru>
2019-07-02 18:44:31 +0400

    Добавил параметры миграции для корректной работы с alembic.

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from sqlalchemy import MetaData
 
 from config import Config
 
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
 cors = CORS()
 jwt = JWTManager()
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 api = Api()
 migrate = Migrate()
 
@@ -30,6 +39,6 @@ def create_app(config_class=Config):
     db.init_app(app)
     jwt.init_app(app)
     api.init_app(app)
-    migrate.init_app(app, db)
+    migrate.init_app(app, db, render_as_batch=True)
 
     return app
